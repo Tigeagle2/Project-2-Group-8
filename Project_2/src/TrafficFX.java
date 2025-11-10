f7c1fa6a7bb6e67d652b86bbac7402149bc383bc
Tigeagle2 <cadendouglas05@gmail.com>
2025-11-10 14:07:32 -0600

    Update TrafficFX.java

--- Project_2/src/TrafficFX.java
+++ Project_2/src/TrafficFX.java
@@ -1,17 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
- */
-
 /**
  *
  * @author tigea
  */
 public class TrafficFX {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String args[]) {
         
     }
